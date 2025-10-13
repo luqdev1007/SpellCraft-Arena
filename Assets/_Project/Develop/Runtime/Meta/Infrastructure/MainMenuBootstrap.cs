e92cd56632b838bc44006be302a726b611466540
LuQmu5 <luqdev1007@gmail.com>
2025-10-13 12:34:45 +0400

    fixed main menu bootstrap: added GameModeSelectionService

--- Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuBootstrap.cs
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuBootstrap.cs
@@ -1,8 +1,5 @@
 ﻿using Assets._Project.Develop.Infrastructure;
 using Assets._Project.Develop.Infrastructure.DI;
-using Assets._Project.Develop.Runtime.Gameplay.TypeMode;
-using Assets._Project.Develop.Runtime.Utilites.ConfigsManagment;
-using Assets._Project.Develop.Runtime.Utilites.CoroutinesManagment;
 using Assets._Project.Develop.Runtime.Utilites.SceneManagement;
 using System.Collections;
 using UnityEngine;
@@ -12,6 +9,7 @@ namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
     public class MainMenuBootstrap : SceneBootstrap
     {
         private DIContainer _container;
+        private GameModeSelectionService _gameModeSelectionService;
 
         public override void ProcessRegistrations(DIContainer container, IInputSceneArgs sceneArgs = null)
         {
@@ -24,34 +22,20 @@ namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
         {
             Debug.Log("Main menu scene init");
 
+            _gameModeSelectionService = _container.Resolve<GameModeSelectionService>();
+
             yield break;
+        }
+
+        private void Update()
+        {
+            if (_gameModeSelectionService != null)
+                _gameModeSelectionService.Update();
         }
 
         public override void Run()
         {
             Debug.Log("Start main menu scene");
-        }
-
-        // delete later
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                SceneSwitcherService sceneSwitcherService = _container.Resolve<SceneSwitcherService>();
-                ICoroutinesPerformer coroutinesPerformer = _container.Resolve<ICoroutinesPerformer>();
-                TypeNumbersGameModeConfig gameMode = _container.Resolve<ConfigsProviderService>().GetConfig<TypeNumbersGameModeConfig>();
-
-                coroutinesPerformer.StartPerform(sceneSwitcherService.ProcessingSwitchTo(Scenes.Gameplay, new GameplayInputArgs(gameMode)));
-            }
-
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                SceneSwitcherService sceneSwitcherService = _container.Resolve<SceneSwitcherService>();
-                ICoroutinesPerformer coroutinesPerformer = _container.Resolve<ICoroutinesPerformer>();
-                TypeCharsGameModeConfig gameMode = _container.Resolve<ConfigsProviderService>().GetConfig<TypeCharsGameModeConfig>();
-
-                coroutinesPerformer.StartPerform(sceneSwitcherService.ProcessingSwitchTo(Scenes.Gameplay, new GameplayInputArgs(gameMode)));
-            }
         }
     }
 }
--- Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
+++ Assets/_Project/Develop/Runtime/Meta/Infrastructure/MainMenuContextRegistrations.cs
@@ -1,4 +1,7 @@
 ﻿using Assets._Project.Develop.Infrastructure.DI;
+using Assets._Project.Develop.Runtime.Utilites.ConfigsManagment;
+using Assets._Project.Develop.Runtime.Utilites.CoroutinesManagment;
+using Assets._Project.Develop.Runtime.Utilites.SceneManagement;
 using UnityEngine;
 
 namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
@@ -8,6 +11,13 @@ namespace Assets._Project.Develop.Runtime.Meta.Infrastructure
         public static void Process(DIContainer container)
         {
             Debug.Log("Process registrations on main menu scene");
+
+            container.RegisterAsSingle(c =>
+               new GameModeSelectionService(
+                   c.Resolve<SceneSwitcherService>(),
+                   c.Resolve<ICoroutinesPerformer>(),
+                   c.Resolve<ConfigsProviderService>()
+               ));
         }
     }
 }
