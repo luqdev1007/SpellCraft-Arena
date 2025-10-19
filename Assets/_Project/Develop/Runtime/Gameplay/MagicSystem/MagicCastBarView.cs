df2dba2d10bf921d2df7bc8af5ae0b42bdd9a99a
LuQmu5 <luqdev1007@gmail.com>
2025-10-19 19:19:53 +0400

    UI magic cast bar

--- Assets/_Project/Develop/Runtime/Gameplay/MagicSystem/MagicCastBarView.cs
+++ Assets/_Project/Develop/Runtime/Gameplay/MagicSystem/MagicCastBarView.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using TMPro;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +6,7 @@ public class MagicCastBarView : MonoBehaviour
 {
     [SerializeField] private Button _openAspectsButton;
     [SerializeField] private AspectButton[] _aspectButtons;
+    [SerializeField] private Image _aspectsArcFilledImage;
 
     private Coroutine _openningCoroutine;
     private bool _openned = false;
@@ -30,17 +29,37 @@ public class MagicCastBarView : MonoBehaviour
         _openningCoroutine = StartCoroutine(Toggling());
     }
 
+    private IEnumerator OpenningArc(float duration, bool isReversed)
+    {
+        float time = 0f;
+        float start = isReversed ? 1f : 0f;
+        float end = isReversed ? 0f : 1f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = Mathf.Clamp01(time / duration);
+            _aspectsArcFilledImage.fillAmount = Mathf.Lerp(start, end, t);
+
+            yield return null;
+        }
+
+        _aspectsArcFilledImage.fillAmount = end;
+    }
+
     private IEnumerator Toggling()
     {
         float openningDelay = 0.025f;
+        float totalDuration = _aspectButtons.Length * openningDelay;
         WaitForSeconds delay = new WaitForSeconds(openningDelay);
 
         if (_openned)
         {
+            StartCoroutine(OpenningArc(totalDuration, true));
+
             for (int i = _aspectButtons.Length - 1; i >= 0; i--)
             {
                 _aspectButtons[i].gameObject.SetActive(false);
-
                 yield return delay;
             }
 
@@ -48,10 +67,11 @@ public class MagicCastBarView : MonoBehaviour
         }
         else
         {
+            StartCoroutine(OpenningArc(totalDuration, false));
+
             for (int i = 0; i < _aspectButtons.Length; i++)
             {
                 _aspectButtons[i].gameObject.SetActive(true);
-
                 yield return delay;
             }
 
