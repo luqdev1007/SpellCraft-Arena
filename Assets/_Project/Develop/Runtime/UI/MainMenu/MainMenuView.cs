ee8b14ba7c34a342b5e06d86bd4ced127433f963
LuQmu5 <luqdev1007@gmail.com>
2025-11-02 22:33:17 +0400

    Main Menu rework begin

---
+++ Assets/_Project/Develop/Runtime/UI/MainMenu/MainMenuView.cs
@@ -0,0 +1,54 @@
+using Assets._Project.Develop.Runtime.UI.Core;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuView : MonoBehaviour, IView
+{
+    public event Action StartGameButtonClicked;
+    public event Action ResetStatsButtonClicked;
+
+    [SerializeField] private IconTextView _goldView;
+    [SerializeField] private IconTextView _winsView;
+    [SerializeField] private IconTextView _losesView;
+
+    [SerializeField] private Button _startGameButton;
+    [SerializeField] private Button _resetStatsButton;
+
+    private void OnEnable()
+    {
+        _startGameButton.onClick.AddListener(OnStartGameButtonClicked);
+        _resetStatsButton.onClick.AddListener(OnResetStatsButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        _startGameButton.onClick.RemoveListener(OnStartGameButtonClicked);
+        _resetStatsButton.onClick.RemoveListener(OnResetStatsButtonClicked);
+    }
+
+    public void SetWinsText(string value)
+    {
+        _winsView.SetText(value);
+    }
+
+    public void SetLosesText(string value)
+    {
+        _losesView.SetText(value);
+    }
+
+    public void SetGoldText(string value)
+    {
+        _goldView.SetText(value);
+    }
+
+    private void OnResetStatsButtonClicked()
+    {
+        ResetStatsButtonClicked?.Invoke();
+    }
+
+    private void OnStartGameButtonClicked()
+    {
+        StartGameButtonClicked?.Invoke();
+    }
+}
