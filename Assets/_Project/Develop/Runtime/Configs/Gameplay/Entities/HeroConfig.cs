87ba94d27d0ed789b828f099395f6fcb034cb13e
LuQmu5 <luqdev1007@gmail.com>
2026-03-02 18:41:46 +0400

    Скорость анимации к скорости атаки фича

--- Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/HeroConfig.cs
+++ Assets/_Project/Develop/Runtime/Configs/Gameplay/Entities/HeroConfig.cs
@@ -8,11 +8,45 @@ namespace Assets._Project.Develop.Runtime.Configs.Gameplay.Entities
         [field: SerializeField] public string PrefabPath { get; private set; } = "Entities/Hero";
         [field: SerializeField, Min(0)] public float MoveSpeed { get; private set; } = 10;
         [field: SerializeField, Min(0)] public float RotationSpeed { get; private set; } = 900;
+
+        [Header("Attack Settings")]
         [field: SerializeField, Min(0)] public float AttackProcessTime { get; private set; } = 1.5f;
         [field: SerializeField, Min(0)] public float AttackDelayTime { get; private set; } = 0.75f;
+
         [field: SerializeField, Min(0)] public float AttackCooldown { get; private set; } = 1f;
         [field: SerializeField, Min(0)] public float InstantAttackDamage { get; private set; } = 50;
         [field: SerializeField, Min(0)] public float MaxHealth { get; private set; } = 100;
         [field: SerializeField, Min(0)] public float DeathProcessTime { get; private set; } = 2;
+
+        [SerializeField, HideInInspector] private float _lastAttackProcessTime;
+        [SerializeField, HideInInspector] private float _lastAttackDelayTime;
+
+        private void OnValidate()
+        {
+            if (_lastAttackProcessTime <= 0)
+            {
+                _lastAttackProcessTime = AttackProcessTime;
+                _lastAttackDelayTime = AttackDelayTime;
+                return;
+            }
+
+            if (!Mathf.Approximately(AttackProcessTime, _lastAttackProcessTime))
+            {
+                if (_lastAttackProcessTime > 0)
+                {
+                    float ratio = _lastAttackDelayTime / _lastAttackProcessTime;
+                    AttackDelayTime = AttackProcessTime * ratio;
+                }
+
+                _lastAttackProcessTime = AttackProcessTime;
+                _lastAttackDelayTime = AttackDelayTime;
+            }
+
+            else if (!Mathf.Approximately(AttackDelayTime, _lastAttackDelayTime))
+            {
+                _lastAttackDelayTime = AttackDelayTime;
+                _lastAttackProcessTime = AttackProcessTime;
+            }
+        }
     }
 }
---
+++ Assets/_Project/Develop/Runtime/Gameplay/Features/Attack/AttackAnimationSpeedView.cs
@@ -0,0 +1,33 @@
+﻿using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore.Mono;
+using Assets._Project.Develop.Runtime.Gameplay.EntitiesCore;
+using Assets._Project.Develop.Runtime.Utilites.Reactive;
+using UnityEngine;
+
+namespace Assets._Project.Develop.Runtime.Gameplay.Features.Attack
+{
+    [RequireComponent(typeof(Animator))]
+    public class AttackAnimationSpeedView : EntityView
+    {
+        private readonly int _attackAnimationSpeedMultiplierKey = Animator.StringToHash("AttackAnimationSpeedMultiplier");
+
+        [SerializeField] private AnimationClip _animationClip;
+        [SerializeField] private Animator _animator;
+
+        private ReactiveVariable<float> _attackProcessTime;
+
+        private void OnValidate()
+        {
+            _animator ??= GetComponent<Animator>();
+        }
+
+        protected override void OnEntityStartedWork(Entity entity)
+        {
+            _attackProcessTime = entity.AttackProcessInitialTime;
+
+            // Формула: Длина анимации / Время процесса в логике = Множитель скорости для Animator
+            float speedMultiplier = _animationClip.length / _attackProcessTime.Value;
+
+            _animator.SetFloat(_attackAnimationSpeedMultiplierKey, speedMultiplier);
+        }
+    }
+}
