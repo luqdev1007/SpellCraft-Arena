2e570d6ea46efe8da7ffd5ed7dd2c788e89069dd
LuQmu5 <luqdev1007@gmail.com>
2026-03-05 22:42:20 +0400

    штше

--- Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
+++ Assets/_Project/Develop/Editor/EntryPointSceneAutoLoader.cs
@@ -1,18 +1,50 @@
-using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEditor;
 
 namespace Assets._Project.Develop.Editor
 {
     [InitializeOnLoad]
     public static class EntryPointSceneAutoLoader
     {
+        private const string MenuPath = "PlayFromBootstrap/Enabled";
+        private const string PlayFromBootstrapKey = "PlayFromBootstrapKey";
+
         static EntryPointSceneAutoLoader()
         {
-            if (EditorBuildSettings.scenes.Length == 0)
-                return;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
 
-            EditorSceneManager.playModeStartScene = AssetDatabase
-                .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode)
+            {
+                if (EditorPrefs.GetBool(PlayFromBootstrapKey) == false)
+                {
+                    EditorSceneManager.playModeStartScene = null;
+                    return;
+                }
+
+                if (EditorBuildSettings.scenes.Length == 0)
+                    return;
+
+                EditorSceneManager.playModeStartScene = AssetDatabase
+                    .LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[0].path);
+            }
+        }
+
+
+        [MenuItem(MenuPath)]
+        private static void Toggle()
+        {
+            bool result = EditorPrefs.GetBool(PlayFromBootstrapKey);
+            EditorPrefs.SetBool(PlayFromBootstrapKey, !result);
+        }
+
+        [MenuItem(MenuPath, true)]
+        private static bool ToggleValidate()
+        {
+            Menu.SetChecked(MenuPath, EditorPrefs.GetBool(PlayFromBootstrapKey));
+            return true;
         }
     }
 }
